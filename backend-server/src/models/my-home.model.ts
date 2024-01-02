e094f5c2b64a893afb5c5e370ebc6939b686abdf
Joseph Dinh <josephdinh@josephs-mbp.lan>
2024-01-02 11:00:01 -0500

    separate backend code from this repository

--- backend-server/src/models/my-home.model.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import HomeSchema from '../schemas/home.schema';
-
-const myHomeSchema = new mongoose.Schema(HomeSchema);
-
-const MyHome = mongoose.model('MyHome', myHomeSchema);
-
-export default MyHome;
